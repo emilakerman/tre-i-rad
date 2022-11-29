bb14d5a359483b4194597f14492e331d78a67a1a
Emil Åkerman <emil@Emils-MacBook-Air.local>
2022-11-29 11:13:35 +0100

    first commit

---
+++ Tre-i-rad/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tre-i-rad
+//
+//  Created by Emil Åkerman on 2022-11-29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.////
+    }
+
+
+}
+
---
+++ Tre-i-radUITests/Tre_i_radUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Tre_i_radUITestsLaunchTests.swift
+//  Tre-i-radUITests
+//
+//  Created by Emil Åkerman on 2022-11-29.
+//
+
+import XCTest
+
+final class Tre_i_radUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
